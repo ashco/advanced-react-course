cad3a10e538cefd4dc213f8d9b8379f85fe25fb0
Ashton <ashchristie@gmail.com>
2018-11-21 16:06:33 -0800

    setting up query / mutation resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,29 @@
-const Mutations = {};
+const Mutations = {
+  // // Must mirror what's in schema
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog!
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  //   // console.log(args);
+  // },
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if logged in
+
+    // This is a Promise
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    // Return the promise
+    return item;
+  },
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,16 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+// Entries in here are called resolvers. Resolvers are how we declare how the server can interact with Prisma DB
+const Query = {
+  // below is shorthand for items: function (args) {}
+  // async items(parent, args, ctx, info) {
+  //   console.log('Getting Items!');
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
+  // If query is exactly the same on Yoga as it is on Prisma, you can forward query...
+  // sends query straight to Prisma server, note that it doesn't allow for auth and stuff
+  items: forwardTo('db'),
+};
 
 module.exports = Query;
