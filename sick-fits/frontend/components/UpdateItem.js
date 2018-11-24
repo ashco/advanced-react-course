10995fe634d834b15a3df8d61d12ca156c9c37fe
Ashton <ashchristie@gmail.com>
2018-11-23 23:27:20 -0800

    updating items

---
+++ sick-fits/frontend/components/UpdateItem.js
@@ -0,0 +1,144 @@
+﻿import React, { Component } from 'react';
+import { Mutation, Query } from 'react-apollo';
+import Router from 'next/router';
+import gql from 'graphql-tag';
+
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+
+import Error from './ErrorMessage';
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+// How form will update data
+const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    # Arguments to take
+    $id: ID!
+    $title: String
+    $description: String
+    $price: Int
+  ) {
+    # run createItem mutation that was defined in backend
+    updateItem(
+      # use provided $variable as own name
+      id: $id
+      title: $title
+      description: $description
+      price: $price
+    ) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {};
+
+  // Very robust handle change func
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    // console.log({ name, type, value });
+    // Convert strings to numbers when type is number
+    const val = type === 'number' ? parseFloat(value) : value;
+
+    this.setState({ [name]: val });
+  };
+
+  updateItem = async (e, updateItemMutation) => {
+    e.preventDefault();
+    // Only items that are changed will be recorded in state, so only those will be updated
+    console.log('Updating Item!');
+    console.log(this.state);
+
+    const res = await updateItemMutation({
+      variables: {
+        id: this.props.id,
+        ...this.state,
+      },
+    });
+    console.log('Updated!');
+  };
+
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ data, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item Found for ID: {this.props.id}</p>;
+          return (
+            // Can pass in variables in component
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {(updateItem, { loading, error }) => (
+                //* how to pass methods to component methods
+                <Form onSubmit={e => this.updateItem(e, updateItem)}>
+                  {/* Only shows error if there is an error */}
+                  <Error error={error} />
+                  {/* fieldset is cool because you can set it to disabled and prevent form from being used / submitting new data */}
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        //* defaultValue sets 1 time value to input w/o tying it to state
+                        defaultValue={data.item.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        defaultValue={data.item.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Enter A Description"
+                        required
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <button type="submit">
+                      Sav{loading ? 'ing' : 'e'} Changes
+                    </button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default UpdateItem;
+export { UPDATE_ITEM_MUTATION };
---
+++ sick-fits/frontend/pages/update.js
@@ -0,0 +1,10 @@
+﻿import UpdateItem from '../components/UpdateItem';
+
+// url query (?id=cjov2lgtuag610a7148gcpo0p&ash=dope) will pass down props (ash: dope)
+const Update = ({ query }) => (
+  <div>
+    <UpdateItem id={query.id} />
+  </div>
+);
+
+export default Update;
