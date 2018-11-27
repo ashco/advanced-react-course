7aef463538be39eaa0594f095d15f58e60970c90
Ashton <ashchristie@gmail.com>
2018-11-26 19:41:57 -0800

    added in password reset on frontend

---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,10 @@
+﻿import Reset from '../components/Reset';
+
+const ResetPage = props => (
+  <div>
+    <p>Reset your password </p>
+    <Reset resetToken={props.query.resetToken} />
+  </div>
+);
+
+export default ResetPage;
