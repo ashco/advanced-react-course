f2de2a3496826a5c769479d86c89c37213451270
Ashton <ashchristie@gmail.com>
2018-12-15 20:21:11 -0800

    order schema

--- sick-fits/frontend/config.js
+++ sick-fits/frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const perPage = 4;
-export const prodEndpoint = `add in url here`;
+export const prodEndpoint = `https://sick-fits-and-yoga-prod2.herokuapp.com/`;
